71f72dc144611027a8cea34846f12081aa8f62b9
YaRo <roman.yatsenko@hneu.net>
2025-04-09 18:38:11 +0300

    Add course mixins and views

--- courses/views.py
+++ courses/views.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views.generic.list import ListView
+
+from .mixins import OwnerCourseEditMixin, OwnerCourseMixin
 
 # Create your views here.
-
 
 class HomeView(TemplateView):
 
     template_name = "base.html"
+
+
+class ManageCourseListView(OwnerCourseMixin, ListView):
+    template_name = 'courses/manage/course/list.html'
+
+
+class CourseCreateView(OwnerCourseEditMixin, CreateView):
+    pass
+
+
+class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
+    pass
+
+
+class CourseDeleteView(OwnerCourseMixin, DeleteView):
+    template_name = 'courses/manage/course/delete.html'
