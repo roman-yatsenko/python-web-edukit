257e59c4247b60e971c7193a54c38d92676d862c
YaRo <roman.yatsenko@hneu.net>
2025-06-11 20:03:04 +0300

    Add CourseSerializer

--- courses/api/serializers.py
+++ courses/api/serializers.py
@@ -1,7 +1,19 @@
 from rest_framework import serializers
 
-from courses.models import Subject
+from courses.models import Course, Module, Subject
 
+
+class ModuleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Module
+        fields = ['order', 'title', 'description']
+
+class CourseSerializer(serializers.ModelSerializer):
+    modules = ModuleSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Course
+        fields = ['id', 'subject', 'title', 'slug', 'overview', 'created', 'owner', 'modules']
 
 class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
